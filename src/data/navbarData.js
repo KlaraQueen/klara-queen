7a872826852e9f8f4565d87fdf68e6bfed87f007
Karolina Kulpinska <kulpinska.karolinaa@gmail.com>
2026-04-07 23:53:33 +0200

    Ultra safe deploy setup

--- src/data/navbarData.js
+++ src/data/navbarData.js
@@ -2,6 +2,7 @@ import { socialMediaData } from "./socialMediaData";
 
 export const navbarData = {
   labels: [
+    { name: "Start", to: "/klara-queen" },
     { name: "Oferta", to: "/offer" },
     { name: "Aktualności", to: "/blog" },
     { name: "Współpraca", to: "/cooperation" },
